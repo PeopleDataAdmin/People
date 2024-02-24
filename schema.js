15c21d37ee353ba656dd45dee319971ba7f544d7
PeopleDataAdmin <161153696+PeopleDataAdmin@users.noreply.github.com>
2024-02-24 13:16:21 -0700

    Update schema.js

--- schema.js
+++ schema.js
@@ -1,8 +1,9 @@
 "User": {
   "id": "string", // Unique identifier for the user. Example: "U123" people.org/user/id/u123 - brings up user profile if viewer attributes match object action condition. // Routing: GET /users/{userId} to retrieve user information
+  "type" person, organization, group
   "firstName": "string", // User's first name. Example: "Cam"
   "lastName": "string", // User's last name. Example: "Alot"
-  "email": "string", // User's email address. Example: "camalot999@gmail.com" // Routing: POST /users/{userId}/update to update user's basic information
+  "email": "string", // User's email address. Example: "camalot999@gmail.com" // Routing: https://api.people.org/users/{userId}/update to update user's basic information
   "phone": "string", // User's phone number. Example: "+1234567890"
   "profilePictureUrl": "string", // URL to the user's profile picture. Example: "https://example.com/profile.jpg"
   "backgroundPictureUrl": "string", // URL to the user's background picture. Example: "https://example.com/background.jpg"
@@ -11,6 +12,22 @@
     "latitude": "float", // Latitude part of the user's current location. Example: 40.0149856
     "longitude": "float", // Longitude part of the user's current location. Example: -105.2705456
     "description": "string" // Human-readable location description. Example: "Boulder, CO" // Routing: PATCH /users/{userId}/location to update user's current location
-}
+  "platformData" // data and schema from connectd platforms
+      "polly"
+        "votingHistory": 
+          "questionID":
+          "question": of these three, how likely are you to vote for each?
+            "responses":
+              "name": Kennedy
+              "sliderResponse": 3.5
+              "name": Biden
+              "sliderResponse": -5
+        "associatedPolIdentities": (progressive, libertarian, independant),
+        "subculturalIdentity": (hipster, punk, goth, nerd)
 
-}
+
+"Platform"
+  "ONELocal"
+
+Objects
+    "type": resource, skill, role, task, project, event, person, organization, group, question, vote, comment/reply, post, chatMessage, Poll
